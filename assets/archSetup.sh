4480ab594353de75ef221482a9a371e8af0056a0
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-06-04 11:55:47 +0000

    Update archSetup.sh

--- assets/archSetup.sh
+++ assets/archSetup.sh
@@ -1,3 +1,15 @@
+echo "starting bluetooth system service"
 sudo systemctl start bluetooth.service
+echo "bluetooth system service started"
+echo "enabling bluetooth system service"
 sudo systemctl enable bluetooth.service
+echo "bluetooth system service enabled"
+echo "installing yay"
+cd ~/Downloads
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+echo "yay installed"
 echo "done"
