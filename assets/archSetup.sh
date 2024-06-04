8ea58450fd4a63bfd73e84fbf9bc542fa626e6bd
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-06-04 12:54:52 +0000

    Update archSetup.sh

--- assets/archSetup.sh
+++ assets/archSetup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Function to print text in bright blue
+print_bright_blue() {
+    echo -e "\033[1;34m$1\033[0m"
+}
+
 # System services, starting and enabling them
 echo "starting bluetooth system service"
 sudo systemctl start bluetooth.service
@@ -5,16 +12,18 @@ echo "bluetooth system service started"
 echo "enabling bluetooth system service"
 sudo systemctl enable bluetooth.service
 echo "bluetooth system service enabled"
+
 # Installing yay
 echo "installing yay"
-cd ~/Downloads
+cd ~/Downloads || exit
 git clone https://aur.archlinux.org/yay.git
-cd yay
+cd yay || exit
 {
-  printf '\n'
-} | makepkg -si
+    printf 'y\n'  # Send 'y' and Enter for the [Y/n] prompt
+} | makepkg -si --noconfirm
 cd ..
 rm -rf yay
 echo "yay installed"
+
 # Flagging script as being concluded
-echo -e "\033[1;34mSetup Finished ;)\033[0m"
+print_bright_blue "Setup Finished ;)"
