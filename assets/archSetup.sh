d4e06c5c4ddd4e8e4da2b76e3a4c7f43cb731075
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-06-04 14:31:57 +0000

    Update archSetup.sh

--- assets/archSetup.sh
+++ assets/archSetup.sh
@@ -27,7 +27,7 @@ rm -rf yay
 echo "yay installed"
 
 # Install yay packages
-echo -e 'A\nN\n' | yay -S drawio clion github-desktop librewolf-bin nordvpn-bin pycharm-professional qflipper zoom --noconfirm
+echo -e 'A\nN\n' | yay -S drawio clion github-desktop librewolf-bin nordvpn-bin pycharm-professional qflipper visual-studio-code zoom --noconfirm
 
 
 # Flagging script as being concluded
