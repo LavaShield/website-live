c6694ff191fcf40d2d76d92a75c5ff9bd3ded46e
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-06-05 21:35:48 +1000

    Update nixsetup.sh
    nix ackages

--- scripts/nixsetup.sh
+++ scripts/nixsetup.sh
@@ -6,13 +6,21 @@ print_bright_blue() {
 }
 
 # Installing nix packages
-packages="audacity bitwarden-desktop cheese deja-dup drawio filezilla firefox flameshot //
+echo "Installing Nix Packages..."
+packages="audacity bitwarden-desktop cheese deja-dup drawio filezilla flameshot //
           freecad gimp git github-desktop jetbrains.clion jetbrains.pycharm-professional //
           libreoffice-fresh librewolf neofetch obs-studio python3 qbittorrent qFlipper //
           signal-desktop sqlitebrowser steam thunderbird timeshift veracrypt vim //
           virtualbox vlc vscode wireshark" # Add more packages separated by spaces
 nix_packages="nixos.$(echo $packages | sed 's/ / nixos./g')"
 nix-env -iA $nix_packages
+echo "Nix Packages Installed."
 
 # Flagging script as being concluded
 print_bright_blue "Setup Finished ;)"
+
+# archlinux package Δ's
+# + cheese deja-dup
+# - bluez bluez-utils cinnamon cups dosfstools exfat-utils gnome 
+#   gnome-extra hplip nordvpn-bin ntfs-3g nvidia nvidia-settings 
+#   nvidia-utils p7zip system-config-printer zoom
