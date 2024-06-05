887524287e66e457f2b98ef2a3ba7818225e665e
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-06-05 21:23:14 +1000

    Update nixsetup.sh
    added packages

--- scripts/nixsetup.sh
+++ scripts/nixsetup.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+# Function to print text in bright blue
+print_bright_blue() {
+    echo -e "\033[1;34m$1\033[0m"
+}
+
+# Installing nix packages
+packages="audacity bitwarden-desktop cheese deja-dup drawio filezilla firefox flameshot //
+          freecad gimp git github-desktop jetbrains.clion jetbrains.pycharm-professional //
+          libreoffice-fresh librewolf neofetch obs-studio python3 qbittorrent qFlipper //
+          signal-desktop sqlitebrowser steam thunderbird timeshift veracrypt vim //
+          virtualbox vlc vscode wireshark" # Add more packages separated by spaces
+nix_packages="nixos.$(echo $packages | sed 's/ / nixos./g')"
+nix-env -iA $nix_packages
+
+# Flagging script as being concluded
+print_bright_blue "Setup Finished ;)"
