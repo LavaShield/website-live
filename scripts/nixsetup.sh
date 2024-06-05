0251be968a62e1f75d95519154941398fb9259cd
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-06-06 00:07:21 +1000

    Update nixsetup.sh
    fucking no dejadup

--- scripts/nixsetup.sh
+++ scripts/nixsetup.sh
@@ -10,7 +10,7 @@ export NIXPKGS_ALLOW_UNFREE=1
 
 # Installing nix packages
 echo "Installing Nix Packages..."
-packages="audacity bitwarden-desktop gnome.cheese deja-dup drawio filezilla flameshot
+packages="audacity bitwarden-desktop gnome.cheese drawio filezilla flameshot
           freecad gimp git github-desktop gnome.gnome-system-monitor htop jetbrains.clion jetbrains.pycharm-professional
           libreoffice-fresh librewolf neofetch obs-studio python3 qbittorrent qFlipper
           signal-desktop sqlitebrowser steam thunderbird timeshift veracrypt vim
