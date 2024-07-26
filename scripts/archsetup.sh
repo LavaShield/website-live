ee15a8a29e2e44e86d8e2a9a892bebca53cc99d4
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-07-26 02:49:32 +0000

    Update archsetup.sh
    added love

--- scripts/archsetup.sh
+++ scripts/archsetup.sh
@@ -7,7 +7,7 @@ general_Packages="base-devel cinnamon curl dosfstools exfat-utils gcc git gnome
 general_Aur_Packages="backintime librewolf-bin nordvpn-bin"
 desktop_Packages="audacity bitwarden bluez bluez-utils code cups 
                   filezilla flameshot freecad gimp hplip 
-                  libreoffice-fresh neofetch obs-studio
+                  libreoffice-fresh love neofetch obs-studio
                   qbittorrent signal-desktop sqlitebrowser steam
                   system-config-printer thunderbird veracrypt vlc wireshark-qt"
 desktop_Aur_Packages="drawio github-desktop minecraft-launcher pycharm-professional 
