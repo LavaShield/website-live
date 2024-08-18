96fcbf3a7a7c34bf48b4afde437e572947988230
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-08-18 09:05:16 +0000

    Update archsetup.sh
    changed to jdk21

--- scripts/archsetup.sh
+++ scripts/archsetup.sh
@@ -2,8 +2,8 @@
 
 # Package Groups
 general_Packages="base-devel cinnamon curl dosfstools exfat-utils gcc git gnome 
-                  gnome-extra htop jdk-openjdk nodejs ntfs-3g openjdk-doc 
-                  openjdk-src p7zip php python tailscale timeshift tmux gvim 
+                  gnome-extra htop jdk21-openjdk nodejs ntfs-3g openjdk21-doc 
+                  openjdk21-src p7zip php python tailscale timeshift tmux gvim 
                   nvidia nvidia-settings nvidia-utils"
 general_Aur_Packages="backintime librewolf-bin nordvpn-bin"
 desktop_Packages="audacity bitwarden bluez bluez-utils code cups 
