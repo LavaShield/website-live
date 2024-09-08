8dafcf5901c08fea1dab8e6e6f5442428f25fa9b
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-09-08 04:54:11 +0000

    Update and rename freebsdsetup.txt to freebsdsetup.sh

---
+++ scripts/freebsdsetup.sh
@@ -0,0 +1,18 @@
+# --- INSTALL CHOICES START ---
+# KEYMAPPING:
+# >>> Continue with default keymap for US keyboard format
+# >>> Pick name for computer in accordance with standards
+# COMPONENTS:
+# >>> Have components kernel-dbg, lib32, ports, src
+# PARTITIONING:
+# >>> (for 1 storage systems, no raid)
+# >>> Auto (ZFS)
+# >>> ZFS Configurations
+# Pool Type/Disks: stripe: 1 disk
+# Pool Name: zroot
+# Force 4K Sectors?: YES (if you are installing on a system with relatively modern components)
+# Encrypt Disks?: YES (always encrypt your devices)
+# Partition Scheme: GPT (UEFI) (should be fine with all standard modern devices)
+# Swap Size: ram size (really up to you, I personally like to make it the same as ram size for hybernation)
+#
+# --- INSTALL CHOICES END ---
