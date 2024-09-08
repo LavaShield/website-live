62f7c75544eff9a6d96f6905edfc8bd84d708590
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-09-08 05:58:04 +0000

    Update freebsdsetup.sh

--- scripts/freebsdsetup.sh
+++ scripts/freebsdsetup.sh
@@ -1,4 +1,4 @@
-# below are the choices to chose when setting up your system with the installer usb
+# below are the choices to chose when setting up your system with the installer usb in the context of a modern laptop
 # --- INSTALL CHOICES START ---
 #
 # KEYMAPPING:
@@ -31,7 +31,7 @@
 # TIME ZONE SELECTOR:
 # >>> Select your timezone :O
 #
-# SYSTEM CONFIGURATION (for a modern laptop):
+# SYSTEM CONFIGURATION:
 # local_unbound: No (Optional; DNS caching typically not needed for most users.)
 # sshd: No (yes if remote access to your machine is needed; otherwise No to enhance security.)
 # moused: No (yes if using the mouse in command-line environments; otherwise No.)
@@ -39,4 +39,17 @@
 # ntpd_sync_on_start: Yes (for immediate synchronization at ntpd start; otherwise No if periodic synchronization suffices.)
 # powerd: Yes (for managing power and extending battery life.)
 # dumpdev: Yes (if you want crash dumps for debugging; otherwise No if not needed or space is limited.)
+#
+# SYSTEM HARDENING:
+# hide_uids: Yes (Provides additional security by hiding user IDs from non-privileged users, reducing the risk of information leakage about system accounts.)
+# hide_gids: Yes (Enhances security by hiding group IDs from non-privileged users, limiting exposure of system group information.)
+# hide_jail: No (Not typically necessary for a regular laptop user. This option is more relevant for systems using jails where you want to obscure their presence.)
+# read_msgbuf: Yes (Prevents non-privileged users from accessing kernel messages, which could contain sensitive system information.)
+# proc_debug: No (Restricts access to process information, which might interfere with debugging tools. Generally not needed for regular users unless you have specific debugging needs.)
+# random_pid: Yes (Improves security by randomizing process IDs, making it harder for attackers to predict and exploit process IDs.)
+# clear_tmp: Yes (Clears temporary files on boot, helping to remove potentially sensitive data and reduce clutter.)
+# disable_syslogd: No (Disabling system logging is not recommended as it is crucial for monitoring and troubleshooting. Properly configuring syslogd is preferred.)
+# secure_console: Yes (Adds an extra layer of protection to the console, preventing unauthorized access to critical system points.)
+# disable_ddtrace: No (Generally unnecessary to disable unless you have specific concerns. DTrace can be useful for performance monitoring and debugging.)
+#
 # --- INSTALL CHOICES END ---
