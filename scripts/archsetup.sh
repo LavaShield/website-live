b2893db8f85818e60d13db6a6815e32ff037ca8c
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-09-11 06:52:19 +0000

    Update archsetup.sh
    cut fat

--- scripts/archsetup.sh
+++ scripts/archsetup.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# how to collect private info
+# read -sp "Enter your password: " user_password
+# echo
+
+
 # Package Groups
 general_Packages="base-devel cinnamon curl dosfstools exfat-utils gcc git gnome 
-                  gnome-extra htop jdk21-openjdk love lua luajit luarocks man-db man-pages nodejs ntfs-3g openjdk21-doc 
-                  openjdk21-src p7zip php python tailscale timeshift tmux gvim 
+                  gnome-extra htop jdk21-openjdk man-db man-pages nodejs ntfs-3g openjdk21-doc 
+                  openjdk21-src p7zip python timeshift tmux gvim 
                   nvidia nvidia-settings nvidia-utils"
 general_Aur_Packages="backintime librewolf-bin nordvpn-bin"
 desktop_Packages="audacity bitwarden bluez bluez-utils code cups 
-                  filezilla flameshot freecad gimp hplip 
-                  libreoffice-fresh neofetch obs-studio opensnitch 
-                  qbittorrent signal-desktop sqlitebrowser steam
+                  filezilla flameshot gimp libreoffice-fresh 
+                  neofetch obs-studio opensnitch 
+                  qbittorrent signal-desktop sqlitebrowser
                   system-config-printer thunderbird veracrypt vlc wireshark-qt"
-desktop_Aur_Packages="brother-hll2460dw drawio minecraft-launcher qflipper"
+desktop_Aur_Packages="qflipper-bin" # brother-hll2460dw
 server_Packages="docker docker-compose openssh ufw"
-server_Aur_Packages=""
+
 
 # Function to print text in bright blue
 print_blue() {
@@ -143,75 +148,3 @@ print_blue "Now for tailscale, go to /usr/lib/sysctl.d/50-default.conf and then 
             \"net.ipv4.conf.all.rp_filter = 1\" (here for more details, https://tailscale.com/download/linux/arch)"
 print_blue "Additionally go to vim and then execute this command ':set clipboard=unnamedplus'"
 print_blue "Now just add flameshot, back in time and timeshift to startup. Maybe a theme???"
-
-
-
-
-
-------------------------- UBUNTU SETUP -------------------------
-#!/bin/bash
-general_apt_packages=""
-general_snap_packages=""
-
-server_apt_packages=""
-server_snap_packages="docker"
-
-desktop_apt_packages="wireshark gimp vlc qbittorrent filezilla flameshot curl"
-desktop_snap_packages="steam thunderbird code bitwarden"
-# --- Install Signal on Desktop Start ---
-# Command 1
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-# Command 2
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-  sudo tee /etc/apt/sources.list.d/signal-xenial.list
-# Command 3
-sudo apt update && sudo apt install signal-desktop
-# --- Install Signal on Desktop End ---
-# --- Install LibreWolf on Desktop Start ---
-
-# --- Install LibreWolf on Desktop End ---
-
-
-# ------------------------------------------------------------------------
-general_Packages="exfat-utils gcc git 
-                  htop nodejs ntfs-3g p7zip php python tailscale timeshift tmux 
-                  gvim nvidia nvidia-settings nvidia-utils"
-general_Aur_Packages="backintime librewolf-bin nordvpn-bin"
-desktop_Packages="audacity cups 
-                  freecad hplip 
-                  obs-studio veracrypt"
-desktop_Aur_Packages="drawio minecraft-launcher qflipper"
-
-
-
-
-------------------------- FREEBSD SETUP -------------------------
-
-echo "SCRIPT START"
-
-read -p "What is your username: " username
-echo "Alight $username, lets do this..."
-
-read -sp "Enter your password: " user_password
-echo
-
-packages="curl bash qtcreator vim libreoffice kmymoney gnucash"
-# the -y flag to pkg install automatically confirms the installation
-pkg install -y $packages
-sysrc kld_list+=linux
-sysrc kld_list+=linux64
-
-
-# --- general ---
-pkg update && pkg upgrade
-pkg install vim
-# --- kernel update ---
-# --- xorg setup ---
-pkg install xorg
-# --- intel graphics ---
-pkg install drm-kmod
-pkg install nvidia-driver
-pkg install cinnamon
-pkg install lightdm lightdm-gtk-greeter
-
