3ec2218f93ce3e28960813bdf901e686132fc349
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-11-05 00:21:07 +0000

    Update mintsetup.sh

--- scripts/mintsetup.sh
+++ scripts/mintsetup.sh
@@ -1,1 +1,5 @@
+# Nix Multi-user setup
+sh <(curl -L https://nixos.org/nix/install) --daemon
 
+# Packages
+nix-env -iA nixpkgs.steam nixpkgs.signal-desktop nixpkgs.kdePackages.kdenlive nixpkgs.flameshot nixpkgs.gimp nixos.backintime
