0d97df00ac4b795b8937de8c791c0daebc3d3a04
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-11-10 17:45:54 +1000

    Delete scripts/mintsetup.sh
    NOT USING ANYMORE

--- scripts/mintsetup.sh
+++
@@ -1,5 +0,0 @@
-# Nix Multi-user setup
-sh <(curl -L https://nixos.org/nix/install) --daemon
-
-# Packages
-nix-env -iA nixpkgs.steam nixpkgs.signal-desktop nixpkgs.kdePackages.kdenlive nixpkgs.flameshot nixpkgs.gimp nixos.backintime
