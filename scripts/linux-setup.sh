0cf51de8fa1bbf516de1e614ebe90f9cf7e26ca0
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-11-10 19:19:02 +1000

    Update linux-setup.sh

--- scripts/linux-setup.sh
+++ scripts/linux-setup.sh
@@ -4,20 +4,18 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 
 
 
-        "base-devel curl dosfstools exfat-utils gcc gdb git htop jdk21-openjdk "
+        "base-devel gcc gdb git htop jdk21-openjdk "
         "man-db man-pages nodejs npm ntfs-3g openjdk21-doc openjdk21-src p7zip python "
-        "rsync timeshift tmux gvim pipewire pipewire-pulse pipewire-alsa reflector "
-        "vulkan-icd-loader vulkan-tools vulkan-validation-layers lib32-vulkan-icd-loader "
-        "lib32-libglvnd mesa lib32-mesa gnome gnome-extra cinnamon"
+        gvim
 
-    "aur_general_packages": "backintime librewolf-bin nordvpn-bin",
+    "aur_general_packages": "backintime librewolf-bin",
 
 
     "desktop_packages": (
-        "audacity bitwarden bluez bluez-utils code cups filezilla flameshot gimp "
+        "audacity  cups filezilla flameshot gimp "
         "libreoffice-fresh neofetch obs-studio opensnitch qbittorrent signal-desktop "
-        "sqlitebrowser system-config-printer thunderbird veracrypt vlc wireshark-qt "
-        "redshift pipewire-jack easyeffects virtualbox virtualbox-host-modules-arch"
+        "sqlitebrowser thunderbird veracrypt vlc wireshark-qt "
+        "virtualbox"
         "kdenlive"
     ),
     "aur_desktop_packages": "qflipper-bin davinci-resolve session-desktop-bin",
@@ -27,6 +25,6 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
     
         
 
-# Additional packages are Bitwarden, vscode, nordvpn, steam and veracrypt. These have been installed already via .deb
+# Additional packages are Bitwarden, vscode, nordvpn, steam, thunderbird and veracrypt. These have been installed already via .deb
 
 # Add flameshot, backintime, and timeshift to startup
