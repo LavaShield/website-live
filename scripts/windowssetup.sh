260946a48f16d0c6d587e271b55cfa4c5de30dae
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-02 22:20:45 +0000

    Update windowssetup.sh

--- scripts/windowssetup.sh
+++ scripts/windowssetup.sh
@@ -1,1 +1,8 @@
+winget install -e --id IDRIX.VeraCrypt Valve.Steam Intel.IntelDriverAndSupportAssistant Zoom.Zoom \
+GIMP.GIMP Audacity.Audacity Bitwarden.Bitwarden OBSProject.OBSStudio Microsoft.Office \
+Mojang.MinecraftLauncher LibreWolf.LibreWolf NordVPN.NordVPN Bitwarden.Bitwarden DolphinEmulator.Dolphin \
+Oxen.Session OpenWhisperSystems.Signal Mozilla.Thunderbird Google.AndroidStudio BlenderFoundation.Blender \
+JetBrains.CLion WiresharkFoundation.Wireshark qBittorrent.qBittorrent GitHub.GitHubDesktop
 
+
+choco install samsung-magician pcsx2 php qflipper youtube-dl-gui
