2da8a487b1dd624bf2d0eba71a0f7060f96785a3
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-02 19:41:24 +0000

    Update ubuntusetup.sh

--- scripts/ubuntusetup.sh
+++ scripts/ubuntusetup.sh
@@ -4,7 +4,8 @@ audacity wireshark firefox love filezilla kdenlive opensnitch vim vlc thunderbir
 qbittorrent dolphin-emu obs-studio backintime-common qflipper dasher timeshift git steam \
 neovim curl android-file-transfer chromium-browser libreoffice python3.13 gimp flameshot \
 freecad htop neofetch virtualbox pcsx2 ruby rsync sqlitebrowser lua5.4 luajit luarocks \
-nodejs php8.3 tmux golang gcc gdb cmake openjdk-21-doc openjdk-21-jdk man-db manpages
+nodejs php8.3 tmux golang gcc gdb cmake openjdk-21-doc openjdk-21-jdk man-db manpages \
+vim-gui-common p7zip
 # Maybe need nvidia nvidiasettings nvidiautils
 
 # Install LibreWolf
@@ -27,4 +28,4 @@ sudo systemctl enable --now opensnitch.service
 echo "Make sure to add flameshot, backintime, and timeshift to startup"
 
 # Installed via .deb: bitwarden, veracrypt, session, minecraft, github-desktop, android-studio, clion, iina, gzdoom, gog-galaxy, nordvpn, vscode
-# intellij, drawio, pycharm, zoom, samsung-magician, crossover, webstorm, youtube-dlg, davinci-resolve
+# intellij, drawio, pycharm, zoom, samsung-magician, crossover, webstorm, youtube-dlg, davinci-resolve, numi
