68dc60d05efc29dc920de9f6be97a89b4c8beb64
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-03 14:31:34 +0000

    Update nixsetup.sh
    addedmore packaheas

--- scripts/nixsetup.sh
+++ scripts/nixsetup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Nix Packages
-PACKAGES="librewolf"
+PACKAGES="librewolf android-file-transfer android-studio rpcs3 audacity backintime bitwarden-desktop blender cemu \
+ungoogled-chromium jetbrains.clion cmake vscode dasher davinci-resolve zoom-us "
 
 # Install Packages
 nix-env -iA $(echo $PACKAGES | sed 's/\b/nixos./g')
+
+echo "Remember To Add Backintime, Flameshot, And Timeshift To Startup!"
