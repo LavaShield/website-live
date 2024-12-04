d12a85b7e972011391fd317cd53f6c9ffb145303
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-04 02:38:13 +0000

    Update nixsetup.sh
    packages

--- scripts/nixsetup.sh
+++ scripts/nixsetup.sh
@@ -6,11 +6,10 @@ ungoogled-chromium jetbrains.clion cmake vscode dasher davinci-resolve zoom-us n
 steam gzdoom vim neovim dolphin-emu session-desktop sqlitebrowser drawio filezilla firefox flameshot freecad \
 jetbrains.webstorm jetbrains.pycharm-community jetbrains.phpstorm thunderbird php84 python313 pcsx2 qflipper \
 gcc gdb gimp git libreoffice yt-dlg wireshark vlc veracrypt timeshift tmux virtualbox ruby qbittorrent lua luajit \
+minecraft-launcher neofetch rsync obs-studio love go kdePackages.kdenlive simple64 github-desktop \
+jetbrains.idea-community jdk"
 
-
-"
-
-# Other Packages: luarocks
+# Other Packages: luarocks, samsung-magician, nordvpn, nvidia, jdk21-src, jdk21-doc
 
 
 # Install Packages
