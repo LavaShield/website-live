27b518560c649f838d665f57c1a586865486589d
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-04 05:58:10 +0000

    Update nixsetup.sh

--- scripts/nixsetup.sh
+++ scripts/nixsetup.sh
@@ -7,11 +7,11 @@ export NIXPKGS_ALLOW_UNFREE=1
 UNFILTERED_PACKAGES="android-file-transfer android-studio audacity backintime bitwarden-desktop blender cemu cmake dasher \
 davinci-resolve dolphin-emu drawio filezilla firefox flameshot freecad gcc gdb gimp git github-desktop gzdoom \
 jetbrains.clion jetbrains.idea-community jetbrains.phpstorm jetbrains.pycharm-community jetbrains.webstorm jdk \
-kdePackages.kdenlive libreoffice librewolf love lua luajit minecraft neofetch nodejs_22 obs-studio \
+kdePackages.kdenlive libreoffice librewolf love lua luajit neofetch nodejs_22 obs-studio \
 opensnitch pcsx2 php84 python313 qbittorrent qflipper rpcs3 rsync session-desktop signal-desktop simple64 \
 sqlitebrowser steam thunderbird timeshift tmux ungoogled-chromium veracrypt vim virtualbox vlc vscode wireshark \
 yt-dlg zoom-us go"
-# Other Packages: luarocks, samsung-magician, nordvpn, nvidia, jdk21-src, jdk21-doc
+# Other Packages: luarocks, samsung-magician, nordvpn, nvidia, jdk21-src, jdk21-doc, minecraft
 
 # Nix Package List Named Properly
 FILTERED_PACKAGES=$(echo "$UNFILTERED_PACKAGES" | sed 's/\b\([^ ]*\)/nixos.\1/g')
