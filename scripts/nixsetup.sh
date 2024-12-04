d2fbba1a7ecb4fcd5dfcf9625cb8cc14ee53ba94
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-04 03:19:17 +0000

    Update nixsetup.sh

--- scripts/nixsetup.sh
+++ scripts/nixsetup.sh
@@ -7,7 +7,7 @@ export NIXPKGS_ALLOW_UNFREE=1
 UNFILTERED_PACKAGES="android-file-transfer android-studio audacity backintime bitwarden-desktop blender cemu cmake dasher \
 davinci-resolve dolphin-emu drawio filezilla firefox flameshot freecad gcc gdb gimp git github-desktop gzdoom \
 jetbrains.clion jetbrains.idea-community jetbrains.phpstorm jetbrains.pycharm-community jetbrains.webstorm jdk \
-kdePackages.kdenlive libreoffice librewolf love lua luajit minecraft-launcher neofetch nodejs_22 obs-studio \
+kdePackages.kdenlive libreoffice librewolf love lua luajit minecraft neofetch nodejs_22 obs-studio \
 opensnitch pcsx2 php84 python313 qbittorrent qflipper rpcs3 rsync session-desktop signal-desktop simple64 \
 sqlitebrowser steam thunderbird timeshift tmux ungoogled-chromium veracrypt vim virtualbox vlc vscode wireshark \
 yt-dlg zoom-us go"
