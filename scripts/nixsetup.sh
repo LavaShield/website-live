1a1fb6a481689f5a1c4fbc4ab7243b73329b40e3
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-04 02:46:20 +0000

    Update nixsetup.sh

--- scripts/nixsetup.sh
+++ scripts/nixsetup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Nix Packages
-PACKAGES="librewolf android-file-transfer android-studio rpcs3 audacity backintime bitwarden-desktop blender cemu \
-ungoogled-chromium jetbrains.clion cmake vscode dasher davinci-resolve zoom-us nodejs_22 opensnitch signal-desktop \
-steam gzdoom vim neovim dolphin-emu session-desktop sqlitebrowser drawio filezilla firefox flameshot freecad \
-jetbrains.webstorm jetbrains.pycharm-community jetbrains.phpstorm thunderbird php84 python313 pcsx2 qflipper \
-gcc gdb gimp git libreoffice yt-dlg wireshark vlc veracrypt timeshift tmux virtualbox ruby qbittorrent lua luajit \
-minecraft-launcher neofetch rsync obs-studio love go kdePackages.kdenlive simple64 github-desktop \
-jetbrains.idea-community jdk"
+PACKAGES="android-file-transfer android-studio audacity backintime bitwarden-desktop blender cemu cmake dasher \
+davinci-resolve dolphin-emu drawio filezilla firefox flameshot freecad gcc gdb gimp git github-desktop gzdoom \
+jetbrains.clion jetbrains.idea-community jetbrains.phpstorm jetbrains.pycharm-community jetbrains.webstorm jdk \
+kdePackages.kdenlive libreoffice librewolf love lua luajit minecraft-launcher neofetch nodejs_22 obs-studio \
+opensnitch pcsx2 php84 python313 qbittorrent qflipper rpcs3 rsync session-desktop signal-desktop simple64 \
+sqlitebrowser steam thunderbird timeshift tmux ungoogled-chromium veracrypt vim virtualbox vlc vscode wireshark \
+yt-dlg zoom-us go"
 
 # Other Packages: luarocks, samsung-magician, nordvpn, nvidia, jdk21-src, jdk21-doc
 
-
-# Install Packages
-nix-env -iA $(echo $PACKAGES | sed 's/\b/nixos./g')
+# Prepend "nixos." to each package and install in one command
+nix-env -iA $(echo $PACKAGES | sed 's/[^ ]\+/"nixos.&"/g' | tr '\n' ' ')
 
 echo "Remember To Add Backintime, Flameshot, And Timeshift To Startup!"
