639344dcfa4644ecfebf7f824f90104616898997
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-05 11:23:46 +0000

    Update w11setup.ps1

--- scripts/w11setup.ps1
+++ scripts/w11setup.ps1
@@ -1,10 +1,10 @@
 # Winget Packages
-winget install -e --id LibreWolf.LibreWolf, Intel.IntelDriverAndSupportAssistantm, qBittorrent.qBittorrent, `
-Google.AndroidStudio, Audacity.Audacity, Bitwarden.Bitwarden, BlenderFoundation.Blender, GitHub.GitHubDesktop, `
-Mozilla.Firefox, Microsoft.VisualStudioCode, VideoLAN.VLC, vim.vim, IDRIX.VeraCrypt, Python.Python.3.12, `
-OpenWhisperSystems.Signal, Oxen.Session, Zoom.Zoom, yt-dlg.yt-dlg, WiresharkFoundation.Wireshark, Valve.Steam `
-Mozilla.Thunderbird, OpenJS.NodeJS.LTS, NordVPN.NordVPN, GOG.Galaxy, Flameshot.Flameshot, FreeCAD.FreeCAD `
-GIMP.GIMP, DolphinEmulator.Dolphin, JGraph.Draw, KDE.Kdenlive, TheDocumentFoundation.LibreOffice, `
+winget install -e --id LibreWolf.LibreWolf, Intel.IntelDriverAndSupportAssistantm, qBittorrent.qBittorrent, ^
+Google.AndroidStudio, Audacity.Audacity, Bitwarden.Bitwarden, BlenderFoundation.Blender, GitHub.GitHubDesktop, ^
+Mozilla.Firefox, Microsoft.VisualStudioCode, VideoLAN.VLC, vim.vim, IDRIX.VeraCrypt, Python.Python.3.12, ^
+OpenWhisperSystems.Signal, Oxen.Session, Zoom.Zoom, yt-dlg.yt-dlg, WiresharkFoundation.Wireshark, Valve.Steam ^
+Mozilla.Thunderbird, OpenJS.NodeJS.LTS, NordVPN.NordVPN, GOG.Galaxy, Flameshot.Flameshot, FreeCAD.FreeCAD ^
+GIMP.GIMP, DolphinEmulator.Dolphin, JGraph.Draw, KDE.Kdenlive, TheDocumentFoundation.LibreOffice, ^
 Mojang.MinecraftLauncher, OBSProject.OBSStudio
 
 echo "Also install the local exe's: nvidia drivers, dotnet-sdk-9, simple64, cemu"
