85342dfa05cacb6c0ad34365e95e62e3966a5033
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-05 09:28:50 +0000

    Update and rename w11setup.sh to w11setup.ps1

---
+++ scripts/w11setup.ps1
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Winget Packages
+winget install -e --id LibreWolf.LibreWolf, Intel.IntelDriverAndSupportAssistantm, qBittorrent.qBittorrent, `
+Google.AndroidStudio, Audacity.Audacity, Bitwarden.Bitwarden, BlenderFoundation.Blender, 
--- scripts/w11setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Winget Packages
-winget install -e --id LibreWolf.LibreWolf Intel.IntelDriverAndSupportAssistant
-qBittorrent.qBittorrent Google.AndroidStudio Audacity.Audacity
