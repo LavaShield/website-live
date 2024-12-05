8e3f9d763904823e152d1d3a383598551841de9a
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-05 10:51:35 +0000

    Update w11setup.ps1

--- scripts/w11setup.ps1
+++ scripts/w11setup.ps1
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Winget Packages
 winget install -e --id LibreWolf.LibreWolf, Intel.IntelDriverAndSupportAssistantm, qBittorrent.qBittorrent, `
 Google.AndroidStudio, Audacity.Audacity, Bitwarden.Bitwarden, BlenderFoundation.Blender, GitHub.GitHubDesktop, `
