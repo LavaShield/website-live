6098ce7ecc4cffc29ac545817ecac10377a9155d
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-06 15:56:03 +0000

    Update nixpkgs.sh

--- scripts/nixpkgs.sh
+++ scripts/nixpkgs.sh
@@ -14,8 +14,7 @@ export NIXPKGS_ALLOW_UNFREE=1
 # Pkgs List
 raw_pkgs="audacity bitwarden-desktop cemu dolphin-emu drawio firefox flameshot freecad gimp github-desktop gzdoom \
 kdePackages.kdenlive librewolf libreoffice minecraft neofetch obs-studio opensnitch pcsx2 python313 qbittorrent rpcs3 \
-ryujinx session-desktop signal-desktop simple64 thunderbird ungoogled-chromium veracrypt vlc \
-vscode yt-dlg vim"
+ryujinx session-desktop simple64 thunderbird ungoogled-chromium veracrypt vlc vscode yt-dlg vim"
 
 # Prepend nixpkgs. in front of all packages and sort alphabetically
 pkgs=$(echo "$raw_pkgs" | tr ' ' '\n' | sort | sed 's/^/nixpkgs./' | tr '\n' ' ')
@@ -27,6 +26,13 @@ nix-env -iA $pkgs
 # Other Packages
 echo "Installing other packages..."
 sudo apt install backintime-qt timeshift steam
+
+# Installing Signal
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop
 
 # Goodbye Message
 echo "Remember to add flameshot, backintime, and timeshift to startup."
