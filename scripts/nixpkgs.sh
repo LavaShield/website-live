8a1e51bbdc81a6dda3de0692794f5e31657cc3bf
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-06 14:04:04 +0000

    Update nixpkgs.sh

--- scripts/nixpkgs.sh
+++ scripts/nixpkgs.sh
@@ -3,7 +3,7 @@
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
 # Pkgs List
-raw_pkgs = "librewolf audacity python313 backintime veracrypt bitwarden-desktop vim qbittorrent gzdoom"
+raw_pkgs = "librewolf audacity python313 backintime veracrypt bitwarden-desktop vim qbittorrent gzdoom cemu"
 
 # Prepend nixpkgs. in front of all packages
 
