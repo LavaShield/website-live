caee10082f89d91bdf0f74b63146fe9bf6037a43
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-19 07:35:22 +0000

    Update mintsetup.sh

--- scripts/mintsetup.sh
+++ scripts/mintsetup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # Preference: 1 - Apt, 2 - Flatpak, 3 - .deb
 
+
 # Apt Packages
 sudo apt install \
 audacity backintime-qt dolphin-emu firefox flameshot freecad gimp kdenlive kiwix libreoffice obs-studio opensnitch \
-python3.13 qbittorrent thunderbird timeshift vim vlc openjdk-21-jdk nodejs
+python3.13 qbittorrent thunderbird timeshift vim vlc openjdk-21-jdk nodejs git
 
 # Flatpak (Flathub) Packages
 flatpak install flathub \
 com.bitwarden.desktop info.cemu.Cemu io.github.ungoogled_software.ungoogled_chromium com.visualstudio.code-oss \
 com.jgraph.drawio.desktop io.github.shiftey.Desktop org.zdoom.GZDoom com.mojang.Minecraft net.rpcs3.RPCS3 \
 org.ryujinx.Ryujinx network.loki.Session io.github.simple64.simple64 net.pcsx2.PCSX2 fr.handbrake.ghb
+
+# Brew Install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Brew Packages
+brew install node@22
 
 # NordVPN Install
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
