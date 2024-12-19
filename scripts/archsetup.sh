f1835a79854c30a12d449d071d32a7d21c3bce55
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-19 23:13:06 +1000

    Update archsetup.sh

--- scripts/archsetup.sh
+++ scripts/archsetup.sh
@@ -1,13 +1,15 @@
-
+#!/bin/bash
 
 # Archlinux Packages
 sudo pacman -Sy \
-bitwarden firefox
+bitwarden firefox signal-desktop audacity
 
 
+# Install Yay
+git clone https://aur.archlinux.org/yay.git ~/Downloads/yay && cd ~/Downloads/yay && makepkg -si --noconfirm && sudo rm -rf ~/Downloads/yay
 
 
-
+# Yay Packages
 
 
 
