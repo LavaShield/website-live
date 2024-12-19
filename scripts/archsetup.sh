d31a402ba2c06d1526e34ea2430f0a9ecca4f33b
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-19 23:24:36 +1000

    Update archsetup.sh

--- scripts/archsetup.sh
+++ scripts/archsetup.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# Archlinux Packages
-sudo pacman -Sy \
-bitwarden firefox signal-desktop audacity
-
-
 # Install Yay
 git clone https://aur.archlinux.org/yay.git ~/Downloads/yay && cd ~/Downloads/yay && makepkg -si --noconfirm && sudo rm -rf ~/Downloads/yay
 
 
+# Archlinux Packages
+sudo pacman -Sy \
+bitwarden firefox signal-desktop audacity chromium dolphin flameshot freecad gimp kdenlive kiwix-desktop libreoffice-fresh \
+obs-studio opensnitch python qbittorrent thunderbird timeshift vim vlc jdk21-openjdk openjdk21-doc openjdk21-src \
+
+
+
 # Yay Packages
+yay -S librewolf nordvpn-bin cemu backintime
 
 
 
 
 
-
-
+# Enable Services
+# sudo systemctl start --now opensnitch.service
+# sudo systemctl enable --now opensnitch.service
 
 
 # Goodbye Message
