57aa495bf9e410049ce124cbc4a8a4091a078c18
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-19 23:30:01 +1000

    Update archsetup.sh

--- scripts/archsetup.sh
+++ scripts/archsetup.sh
@@ -8,11 +8,11 @@ git clone https://aur.archlinux.org/yay.git ~/Downloads/yay && cd ~/Downloads/ya
 sudo pacman -Sy \
 bitwarden firefox signal-desktop audacity chromium dolphin flameshot freecad gimp kdenlive kiwix-desktop libreoffice-fresh \
 obs-studio opensnitch python qbittorrent thunderbird timeshift vim vlc jdk21-openjdk openjdk21-doc openjdk21-src \
-
+nodejs git code drawio-desktop handbrake
 
 
 # Yay Packages
-yay -S librewolf nordvpn-bin cemu backintime
+yay -S librewolf nordvpn-bin cemu backintime github-desktop minecraft-launcher rpcs3-git ryujinx pcsx2-git
 
 
 
