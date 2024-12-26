5498c5ed7c6b76708f1c059c12cdbfaa12753446
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2024-12-26 09:10:03 +0000

    Update mintsetup.sh

--- scripts/mintsetup.sh
+++ scripts/mintsetup.sh
@@ -4,13 +4,13 @@
 
 # Apt Packages
 sudo apt install \
-audacity backintime-qt dolphin-emu firefox flameshot freecad gimp kdenlive kiwix libreoffice obs-studio opensnitch \
+audacity backintime-qt firefox flameshot freecad gimp kdenlive kiwix libreoffice obs-studio opensnitch \
 python3.13 qbittorrent thunderbird timeshift vim vlc git
 
 # Flatpak (Flathub) Packages
 flatpak install flathub \
 com.bitwarden.desktop io.github.ungoogled_software.ungoogled_chromium com.visualstudio.code-oss \
-com.jgraph.drawio.desktop com.mojang.Minecraft net.pcsx2.PCSX2 fr.handbrake.ghb
+com.jgraph.drawio.desktop com.mojang.Minecraft net.pcsx2.PCSX2 fr.handbrake.ghb org.DolphinEmu.dolphin-emu \
 
 # NordVPN Install
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
