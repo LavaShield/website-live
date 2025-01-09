8899a829ab03b12f9685c8f6c890ac8f8b7b3d4a
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2025-01-09 08:49:17 +0000

    Update winsetup.py

--- scripts/winsetup.py
+++ scripts/winsetup.py
@@ -1,6 +1,8 @@
 # Installed via exe
 local_software = "dolphin nvidia-drivers bitwarden intel-dsa librewolf lenovo-service-bridge pcsx2" +
                  "python3 signal veracrypt vscode youtube-dlg qbittorrent nordvpn samsung-magician" +
+                 "firefox thunderbird github-desktop handbrake libreoffice audacity flameshot gimp" +
+                 "obs vlc kiwix microsoft-office drawio"
 
 
 
