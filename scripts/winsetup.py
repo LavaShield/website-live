71c43a8ec16419516671ed4950b1ea628cf0f77f
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2025-01-09 07:56:04 +0000

    Create winsetup.py

---
+++ scripts/winsetup.py
@@ -0,0 +1,16 @@
+# Installed via exe
+local_packages = ""
+
+
+
+
+
+
+print("Packages Installed, Now Do This:",
+      "- Uninstall Onedrive",
+      "- Disable Windows Fast Startup",
+      "- Enable Windows Sandbox",
+      "- Do All Windows Updates",
+      "- Change Device Name",
+      "- Thinkpad Updates From Service Bridge",
+      "- Intel-DSA Updates")
