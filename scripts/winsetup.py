c2c05c1d0f2c3e3814de592726e84fbb993769cd
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2025-01-10 02:49:51 +1000

    GETTING CHOCO

--- scripts/winsetup.py
+++ scripts/winsetup.py
@@ -1,9 +1,20 @@
-# Installed via exe
-local_software = "audacity bitwarden dolphin drawio firefox flameshot gimp github-desktop" +
-                 "handbrake intel-dsa kiwix lenovo-service-bridge libreoffice librewolf" + 
-                 "microsoft-office nordvpn nvidia-drivers obs pcsx2 python3 qbittorrent" + 
+# Installed Locally
+kiwix lenovo-service-bridge microsoft-office nvidia-drivers 
+
+
+
+
+obs pcsx2 python3 qbittorrent" + 
                  "samsung-magician signal thunderbird veracrypt vlc vscode youtube-dlg" +
                  "rufus"
+
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+
+# Install Chocolatey Packages
+choco install audacity bitwarden dolphin drawio firefox flameshot gimp github-desktop handbrake intel-dsa libreoffice-fresh librewolf nordvpn
 
 
 
