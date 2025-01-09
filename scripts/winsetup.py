bcd57c61e0642a87d13787bfff355f8bc2e2dc91
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2025-01-09 08:12:05 +0000

    Update winsetup.py

--- scripts/winsetup.py
+++ scripts/winsetup.py
@@ -1,6 +1,6 @@
 # Installed via exe
 local_software = "dolphin nvidia-drivers bitwarden intel-dsa librewolf lenovo-service-bridge pcsx2" +
-                 "python3 signal veracrypt vscode youtube-dlg qbittorrent"
+                 "python3 signal veracrypt vscode youtube-dlg qbittorrent nordvpn"
 
 
 
