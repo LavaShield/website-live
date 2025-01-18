1c6c7b955d3ca2b1265b8fa0d9d46ac26de718c1
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2025-01-19 06:54:05 +1000

    Add files via upload

---
+++ scripts/macsetup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Brew Install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+# Brew GUI Packages
+brew install --cask \
+audacity bitwarden chromium visual-studio-code crossover dolphin drawio firefox flameshot freecad gimp \
+github handbrake libreoffice librewolf samsung-magician minecraft mos nordvpn obs qbittorrent rectangle \
+signal thunderbird veracrypt vlc wireshark zoom
+
+
+# Brew CLI Packages
+brew install \
+node@22 python@3.13 vim
+
+
+echo "With installer: pcsx2, yt-dlg, kiwix"
+echo "Finished! Installation Complete!"
