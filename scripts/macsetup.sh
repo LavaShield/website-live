6b8e0d4aeb66bd867ac3e3c46a5f6e339cedfd69
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2025-02-17 04:45:45 +0000

    Update macsetup.sh

--- scripts/macsetup.sh
+++ scripts/macsetup.sh
@@ -9,7 +9,7 @@
 brew install --cask \
 audacity visual-studio-code dolphin drawio firefox freecad gimp \
 github handbrake libreoffice samsung-magician minecraft mos nordvpn obs qbittorrent \
-vlc wireshark zoom microsoft-office avogadro roblox \
+vlc wireshark zoom microsoft-office avogadro roblox android-file-transfer \
 yubico-authenticator macos-fuse-t/homebrew-cask/fuse-t
 
 
