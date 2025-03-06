752b5d965bb03941681eb5a375b49a45d97fa479
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2025-03-06 16:42:45 +0000

    Update winsetup.py

--- scripts/winsetup.py
+++ scripts/winsetup.py
@@ -1,12 +1,12 @@
 # Installed Locally
-OculusSetup Nvidia-Drivers NordVPN
+OculusSetup Nvidia-Drivers NordVPN Lenovo-Service-Bridge Lenovo-System-Update
 
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 
 # Install Chocolatey Packages
-choco install dolphin flameshot handbrake intel-dsa librewolf rufus samsung-magician qbittorrent obs-studio pcsx2 python3 vlc vscode youtube-dl-gui steam
+choco install audacity dolphin flameshot handbrake intel-dsa librewolf rufus samsung-magician qbittorrent obs-studio pcsx2 python3 vlc vscode youtube-dl-gui steam
 
 
 
