cbe80b49e750ecbac8ff2565291eeec9be02739f
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2025-03-21 11:36:22 +0000

    Update macsetup.sh

--- scripts/macsetup.sh
+++ scripts/macsetup.sh
@@ -11,7 +11,7 @@ audacity visual-studio-code dolphin drawio firefox freecad gimp \
 github handbrake libreoffice samsung-magician mos nordvpn obs qbittorrent thunderbird \
 vlc wireshark zoom microsoft-office avogadro roblox \
 yubico-authenticator macos-fuse-t/homebrew-cask/fuse-t kdenlive blender \
-intellij-idea-ce
+intellij-idea-ce wine-stable
 
 
 # Brew CLI Packages
